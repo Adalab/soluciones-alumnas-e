e4029c268409f6c490adab03f3611723d1011287
aroatdlt <aroatato@gmx.com>
2018-12-09 12:08:00 +0100

    Add class to remove purple class

--- sprint-2/2.5/ej6/main.js
+++ sprint-2/2.5/ej6/main.js
@@ -6,8 +6,10 @@ const changeColorBody = (e) => {
     
     if (e.key === "r") {
         changeBackgroundColor.classList.add("bgc_red");
+        changeBackgroundColor.classList.remove("bgc_purple");
     } else if (e.key === "m") {
         changeBackgroundColor.classList.add("bgc_purple");
+        changeBackgroundColor.classList.remove("bgc_red");
     } else {
         changeBackgroundColor.classList.remove("bgc_red");
         changeBackgroundColor.classList.remove("bgc_purple");
