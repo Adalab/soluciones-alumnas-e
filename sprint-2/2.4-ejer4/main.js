ae4cb87a83bb515d1b6a9a62349a617cb46109f0
rdfornieles <rdfornieles@gmail.com>
2018-12-09 19:50:06 +0100

    ejer4

---
+++ sprint-2/2.4-ejer4/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function countBox(a, b, c, d) {
+    const borderBox = a;
+
+    if (borderBox === a) {
+        countBox = b;
+    } else {
+        countbox = b, c, d;
+    }
+    
+}
+const resultCountbox = countBox(true, 80, 20, 10);
+console.log(resultCountbox);
