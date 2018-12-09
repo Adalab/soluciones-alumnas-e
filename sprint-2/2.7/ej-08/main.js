0e3abdc85a6d72927b6b52c7bcad970e816b6442
Paloma Ania <paloma.aniabenito@gmail.com>
2018-12-09 11:38:52 +0100

    Change to the right folder

--- sprint-2/2.7/ej-08/main.js
+++ sprint-2/2.7/ej-08/main.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const tasks = [
-    {name: 'Recoger setas en el campo', completed: true},
-    {name: 'Comprar pilas', completed: true},
-    {name: 'Poner una lavadora de blancos', completed: true},
-    {name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript', completed: false}
+    { name: 'Recoger setas en el campo', completed: true },
+    { name: 'Comprar pilas', completed: true },
+    { name: 'Poner una lavadora de blancos', completed: true },
+    { name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript', completed: false }
 ];
 
 let tasksList = document.body.querySelector('#tasks');
