f1f1778a74fed1a8e7da73c1788976682f12e4d3
Beatriz Garcia <beatrizgarcia.jim@gmail.com>
2018-12-11 12:40:10 +0100

    indented

--- sprint-2/2.10/ej-02/main.js
+++ sprint-2/2.10/ej-02/main.js
@@ -1,45 +1,33 @@
 const boxName = document.querySelector(".boxName");
 const btnSearch = document.querySelector(".btn-Search");
 const list = document.querySelector(".list");
-const api= "https://swapi.co/api/people/?search="
+const api = "https://swapi.co/api/people/?search="
 
 
 
-function handlerSearch(e){
+function handlerSearch(e) {
 
-    list.innerHTML="";
+    list.innerHTML = "";
     let valueName = boxName.value;
-    let url = api+valueName;
-    
+    let url = api + valueName;
+
     fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        let characters = data.results;
+        .then(response => response.json())
+        .then(data => {
+            let characters = data.results;
 
-        for (i = 0; i < characters.length; i++){
-            let nameCh= characters[i].name;
-            let gender= characters[i].gender;
-            
-
-            let itemCharacter = document.createElement('li');
-            let newContent = document.createTextNode(`${i+1} ${nameCh} : gender ${gender}`);
-            itemCharacter.appendChild(newContent);
-
-            list.appendChild(itemCharacter);
+            for (i = 0; i < characters.length; i++) {
+                let nameCh = characters[i].name;
+                let gender = characters[i].gender;
 
 
-            console.log(itemCharacter)
-        }
+                let itemCharacter = document.createElement('li');
+                let newContent = document.createTextNode(`${i+1} ${nameCh} : gender ${gender}`);
+                itemCharacter.appendChild(newContent);
 
-
-
-        
-    }
-        
-        
-        
-        
-        )
+                list.appendChild(itemCharacter);
+            }
+        })
 }
 
 
