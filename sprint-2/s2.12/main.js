d5059cebd093256b30a22c0d3131c07a9d882e69
Silvia García Cacho <silvia@garca.info>
2018-12-13 20:19:54 +0100

    Improve ticket/31

--- sprint-2/s2.12/main.js
+++ sprint-2/s2.12/main.js
@@ -10,18 +10,32 @@ const time2 = document.querySelector('.tempor2');
 
 const incrementAndShowCounter = () => {
   counter++
-  if(counter < 60){
-  time.innerHTML = `Escrito hace ${counter} segundos.`;
-  }else if(counter >= 60 && (counter%60) == 0){
+  if(counter === 1){
+    time.innerHTML = `Escrito hace ${counter} segundo.`;
+  }else if(counter < 60){
+    time.innerHTML = `Escrito hace ${counter} segundos.`;
+  }else if(counter === 60){
+    newCounter++;
+    time.innerHTML = `Escrito hace ${newCounter} minuto.`
+  }else if(counter <120 && (counter%60) === 1){
+    let div = counter%60;
+    time.innerHTML = `Escrito hace ${newCounter} minuto y ${div} segundo.`
+  }else if(counter < 120 && (counter%60) != 0){
+    let div = counter%60;
+    time.innerHTML = `Escrito hace ${newCounter} minuto y ${div} segundos.`
+  }else if(counter >= 120 && (counter%60) === 0){
     newCounter++;
     time.innerHTML = `Escrito hace ${newCounter} minutos.`
-  } else if(counter >= 60 && (counter%60) != 0){
+  }else if(counter >= 120 && (counter%60) === 1){
+    let div = counter%60;
+    time.innerHTML = `Escrito hace ${newCounter} minutos y ${div} segundo.`
+  }else if(counter >= 120 && (counter%60) != 0){
     let div = counter%60;
     time.innerHTML = `Escrito hace ${newCounter} minutos y ${div} segundos.`
   }
   time2.innerHTML= counter;
 }
 
-temp = setInterval(incrementAndShowCounter, 1000);
+temp = setInterval(incrementAndShowCounter, 200);
 
 
